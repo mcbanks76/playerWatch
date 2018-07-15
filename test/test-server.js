3cd65f552a3405dc2c2151d463d369442b5fc5ec
Matt Banks <mattkbanks@gmail.com>
2018-07-15 12:46:30 -0400

    working test files with server

--- test/test-server.js
+++ test/test-server.js
@@ -1,18 +1,18 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
 
-// Import server.js and use destructuring assignment to create variables for
-// server.app, server.runServer, and server.closeServer
-const server = require('../server');
-
-// declare a variable for expect from chai import
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('server', function() {
-
-  it('should have status 200', function() {
-    expect('../').to.have.status(200);
+describe("index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
   });
 });
